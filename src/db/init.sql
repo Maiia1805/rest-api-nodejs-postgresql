2b0e134366c8618c97ad5e56662252dc71deeb99
Maiia1805 <masyavchuk@gmail.com>
2026-02-02 20:54:47 +0200

    Create init.sql

---
+++ src/db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    email TEXT
+);
+
+INSERT INTO users(name,email) VALUES('Mike Towers','miketowers@gmail.com');
+INSERT INTO users(name,email) VALUES('Kenneth Johnson','kennethjohnson@gmail.com');
