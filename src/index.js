c547d79f3231040abea4fa6f8e02d0e9892541d0
Maiia1805 <masyavchuk@gmail.com>
2026-02-02 20:14:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
 
-//middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-//routes
 app.use(require('./routes/index'));
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-app.listen(process.env.PORT||4000);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-console.log('Server on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
